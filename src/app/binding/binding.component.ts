c55875b7878fd9bd93adb27b375288d998d36e98
marcos514 <marmarreyer@gmail.com>
2018-08-30 11:41:38 -0300

    Ejersicio

--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -1,26 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , OnChanges , AfterViewInit , OnDestroy ,SimpleChanges } from '@angular/core';
+import { Persona } from '../persona';
 
 @Component({
   selector: 'app-binding',
   templateUrl: './binding.component.html',
   styleUrls: ['./binding.component.css']
 })
-export class BindingComponent implements OnInit {
-  msj="Ingrese datos";
-  msjBtn="Enviar";
-  nombre="Pepe";
+export class BindingComponent implements OnInit,OnChanges,OnDestroy,AfterViewInit {
+  nombre="";
+  apellido="";
+  sueldo="";
+
+  /**
+   * Solo cuando se carga la pagina
+   */
+  ngAfterViewInit(): void {
+    console.log("En after View Init");
+  }
+
+  /**
+   * Cuando cambia algo de la estructura del html se activa
+   * @param changes 
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("En el change");
+  }
+
+  /**
+   * Cuando se destrulle algo del html se activa
+   */
+  ngOnDestroy(): void {
+    console.log("En on destroy");
+  }
+
+  lista=[];
   constructor() { }
 
   /**
    * Enviar
    * @param e: Evento del click 
    */
-  public Enviar(e) 
+  public Enviar() 
   {
-    console.log(e);
+    let persona:Persona=new Persona(this.nombre,this.apellido,this.sueldo);
+    this.lista.push(persona);
+    console.log(this.lista);
+    
   }
 
   ngOnInit() {
+    console.log("En On Init");
   }
 
 }
---
+++ src/app/persona.ts
@@ -0,0 +1,13 @@
+export class Persona {
+    public nombre;
+    public apellido;
+    public sueldo;
+
+    public constructor(nombre:string,apellido:string,sueldo:string)
+    {
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.sueldo=sueldo;
+    }
+
+}
